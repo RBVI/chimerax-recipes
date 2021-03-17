6f5a682202038dc67c1ca26bac3714e6b7c1e262
Tom Goddard <goddard@sonic.net>
2021-03-16 18:12:35 -0700

    Add ellipsoid depiction Python example.

---
+++ ellipsoid/ELM_ellipsoid.py
@@ -0,0 +1,84 @@
+# Draw a rotated ellipsoid, given the values of center of mass, semiaxes, and eigenvectors 
+# written by Matthew Casertano and David Fushman, University of Maryland, June 2019
+from pymol.cgo import BEGIN, COLOR, TRIANGLES, VERTEX, NORMAL, END
+from pymol import cmd
+
+def sin (theta): return math.sin(math.radians(theta)) # sine of an angle in degree form
+def cos (theta): return math.cos(math.radians(theta)) # cosine of an angle in degree form
+
+
+def ellipsoid(cmx, cmy, cmz, a1, a2, a3, u, v, R11, R12, R13, R21, R22, R23, R31, R32, R33):
+ 
+    #coordinates of ellipsoid points and the normals in the ellipsoid coordinate frame 
+        x0 = a1 * cos(u) * cos(v)
+        y0 = a2 * cos(u) * sin(v)
+        z0 = a3 * sin(u)
+        
+        nx0 = x0 / (a1 ** 2)
+        ny0 = y0 / (a2 ** 2)
+        nz0 = z0 / (a3 ** 2)
+
+    #rotate the coordinates of ellipsoid points and the normals into the protein coordinate frame 
+    #and shift the center of ellipsoid to coincide with the COM of the protein molecule 
+
+        x = x0 * R11 + y0 * R12 + z0 * R13 + cmx
+        y = x0 * R21 + y0 * R22 + z0 * R23 + cmy
+        z = x0 * R31 + y0 * R32 + z0 * R33 + cmz
+
+        nx = nx0 * R11 + ny0 * R12 + nz0 * R13
+        ny = nx0 * R21 + ny0 * R22 + nz0 * R23 
+        nz = nx0 * R31 + ny0 * R32 + nz0 * R33 
+
+        return x, y, z, nx, ny, nz
+
+def makeEllipsoid(color, cmx, cmy, cmz, a1, a2, a3, u1, u2, v1, v2, u_segs, v_segs, R11, R12, R13, R21, R22, R23, R31, R32, R33):
+
+        r, g, b = color
+
+        # Calculate delta variables 
+        dU = (u2 - u1) / u_segs
+        dV = (v2 - v1) / v_segs
+
+        o = [BEGIN, TRIANGLES]
+
+        U = u1
+        for Y in range(0, u_segs):
+                # Initialize variables for loop 
+                V = v1
+                for X in range(0, v_segs):
+                    x, y, z, nx, ny, nz = [0] * 5, [0] * 5, [0] * 5, [0] * 5, [0] * 5, [0] * 5 # this allows the array to be filled
+                    for i in range(1, 5): # creates four sets of ellipsoid points and normals, indexed from 1 through 4
+                        x[i], y[i], z[i], nx[i], ny[i], nz[i] = ellipsoid(cmx, cmy, cmz, a1, a2, a3, U + dU * (i == 2 or i == 3), V + dV * (i == 3 or i == 4), R11, R12, R13, R21, R22, R23, R31, R32, R33)
+                    for i in (1, 2, 4, 2, 3, 4): # extends the output list with the new points
+                        o.extend([COLOR, r, g, b, NORMAL, nx[i], ny[i], nz[i], VERTEX, x[i], y[i], z[i]])
+
+                    # Update variables for next loop 
+                    V += dV
+                # Update variables for next loop 
+                U += dU 
+        o.append(END)
+        return o
+
+def drawEllipsoid(color, cmx, cmy, cmz, a1, a2, a3, R11, R12, R13, R21, R22, R23, R31, R32, R33):
+                return makeEllipsoid(color, cmx, cmy, cmz, a1, a2, a3, -90, 90, -180, 180, 10, 10, 
+				R11, R12, R13, R21, R22, R23, R31, R32, R33) 
+
+def rotationMatrix(rotationInput): #Elements of the rotation matrix R = transpose of the rotation matrix defined in Arfken's book
+    if (len (rotationInput)) == 9: return rotationInput # simply return input if it is already a matrix
+    a, b, g = rotationInput[0], rotationInput[1], rotationInput[2]
+    R11, R12, R13 = cos(g) * cos(b) * cos(a) - sin(g) * sin(a), -sin(g) * cos(b) * cos(a) - cos(g) * sin(a), sin(b) * cos(a)
+    R21, R22, R23 = cos(g) * cos(b) * sin(a) + sin(g) * cos(a), -sin(g) * cos(b) * sin(a) + cos(g) * cos(a), sin(b) * sin(a)
+    R31, R32, R33 = -cos(g) * sin(b), sin(g) * sin(b), cos(b)
+    return R11, R12, R13, R21, R22, R23, R31, R32, R33
+ 
+#1. Center of Mass of the molecule
+cmx, cmy, cmz = 20.051, 6.333, -0.915
+#2. Ellipsoid semiaxes length (in Angstrom)
+a1,a2,a3 = 21.700, 24.645, 16.567
+#3. Color: see https://pymolwiki.org/index.php/Color_Values for more options
+color = [0.85, 0.85, 1.00]
+#4. Rotation Input: three Euler angles: alpha, beta, gamma (in degrees)
+rotationInput = [83.790, 114.370, 179.850]
+tmp = drawEllipsoid(color, cmx, cmy, cmz, a1, a2, a3, *rotationMatrix(rotationInput))
+cmd.load_cgo(tmp, 'ellipsoid-cgo')
+cmd.set('cgo_transparency', 0.5, 'ellipsoid-cgo')
---
+++ ellipsoid/ELM_ellipsoid_chimerax.py
@@ -0,0 +1,98 @@
+# Make an ellipsoid depiction in ChimeraX using parameters from
+# ROTDIF software http://gandalf.umd.edu/FushmanLab/pdsw.html
+
+# Parameters on the following 4 lines come from end of
+# PyMol script produced by ROTDIF.
+
+#1. Center of Mass of the molecule
+cmx, cmy, cmz = 20.051, 6.333, -0.915
+#2. Ellipsoid semiaxes length (in Angstrom)
+a1,a2,a3 = 21.700, 24.645, 16.567
+#3. Color: see https://pymolwiki.org/index.php/Color_Values for more options
+color = [0.85, 0.85, 1.00]
+#4. Rotation Input: three Euler angles: alpha, beta, gamma (in degrees)
+rotationInput = [83.790, 114.370, 179.850]
+
+
+
+# Make an ellipsoid showing lattitute and longitude lines.
+# ChimeraX shape sphere command uses smooth spiral triangulation so cannot be used.
+def ellipsoid_mesh(session, name, center, radii, rotation, divisions, color, opacity):
+    vertices, edges = lattitude_longtitude_circles(divisions)
+    
+    # Stretch, rotate and center
+    vertices *= radii
+    rotation.transform_points(vertices, in_place = True)
+    vertices += center
+    
+    # Create ellipsoid model
+    from chimerax.core.models import Surface
+    s = Surface(name, session)
+    normals = None
+    s.set_geometry(vertices, normals, edges)
+    s.color = [int(255*r) for r in color+[opacity]]
+    s.display_style = s.Mesh
+    session.models.add([s])
+
+def lattitude_longtitude_circles(n = 10):
+    cvertices,cedges = unit_circle()
+
+    vertices = []
+    edges = []
+
+    # Make lattitude circles
+    from math import pi, sin, cos
+    for i in range(n):
+        a = pi * (i+1) / (n+1)
+        v,e = cvertices.copy(), cedges.copy()
+        v *= sin(a)
+        v[:,2] += cos(a)
+        e += len(vertices)*len(v)
+        vertices.append(v)
+        edges.append(e)
+
+    # Make longitude circles
+    from chimerax.geometry import vector_rotation
+    for i in range(n):
+        a = pi * i / n
+        v,e = cvertices.copy(), cedges.copy()
+        normal = (cos(a), sin(a), 0)
+        vector_rotation((0,0,1), normal).transform_points(v, in_place = True)
+        e += len(vertices)*len(v)
+        vertices.append(v)
+        edges.append(e)
+
+    from numpy import concatenate
+    return concatenate(vertices), concatenate(edges)
+
+def unit_circle(n = 200):
+    from numpy import empty, float32, int32, sin, cos, arange, pi
+    a = arange(0, 2*pi, 2*pi/n)
+    vertices = empty((n,3), float32)
+    vertices[:,0] = cos(a)
+    vertices[:,1] = sin(a)
+    vertices[:,2] = 0
+    edges = empty((n,2), int32)
+    edges[:,0] = range(n)
+    edges[:,1] = (edges[:,0] + 1) % n
+    return vertices, edges
+
+def euler_rotation(euler_angles):
+    from math import radians, cos, sin
+    a, b, g = [radians(a) for a in euler_angles]
+    R11, R12, R13 = cos(g) * cos(b) * cos(a) - sin(g) * sin(a), -sin(g) * cos(b) * cos(a) - cos(g) * sin(a), sin(b) * cos(a)
+    R21, R22, R23 = cos(g) * cos(b) * sin(a) + sin(g) * cos(a), -sin(g) * cos(b) * sin(a) + cos(g) * cos(a), sin(b) * sin(a)
+    R31, R32, R33 = -cos(g) * sin(b), sin(g) * sin(b), cos(b)
+    from chimerax.geometry import Place
+    rot = Place(axes = ((R11, R12, R13), (R21, R22, R23), (R31, R32, R33)))
+    return rot
+
+# Show ellipsoid in ChimeraX
+ellipsoid_mesh(session,
+               name = 'diffusion ellipsoid',
+               center = (cmx,cmy,cmz),
+               radii = (a1,a2,a3),
+               rotation = euler_rotation(rotationInput),
+               divisions = 10,
+               color = color,
+               opacity = 1.0)
