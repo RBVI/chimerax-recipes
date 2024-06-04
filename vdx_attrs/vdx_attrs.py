9c8a31af7cbabf01373584ddfb871b5c9f0f3f1c
Eric Pettersen <pett@cgl.ucsf.edu>
2024-06-04 14:16:08 -0700

    Updated script to reflect recent changes in ViewDockX

--- vdx_attrs/vdx_attrs.py
+++ vdx_attrs/vdx_attrs.py
@@ -6,25 +6,14 @@ for m in session.models:
 		for k, v in m.viewdockx_data.items():
 			attr_name = string_to_attr(k, prefix='vdx_')
 			if attr_name not in attr_names:
-				try:
-					x = eval(v)
-				except:
-					kw = {}
-					x = v
+				if isinstance(v, (float, int)):
+					kw = { 'attr_type': type(v) }
+					numeric_attr_names.add(attr_name)
 				else:
-					if isinstance(x, (float, int)):
-						kw = { 'attr_type': type(x) }
-						numeric_attr_names.add(attr_name)
-					else:
-						kw = {}
-						x = v
+					kw = { 'attr_type': str }
 				from chimerax.atomic import Structure
 				Structure.register_attr(session, attr_name, "ViewDockX attribute script", **kw)
 				attr_names.add(attr_name)
-			elif attr_name in numeric_attr_names:
-				x = eval(v)
-			else:
-				x = v
-			setattr(m, attr_name, x)
+			setattr(m, attr_name, v)
 session.logger.info("The following numeric ViewDockX attributes were registered: %s" % ", ".join(sorted(list(numeric_attr_names))))
 session.logger.info("The following non-numeric ViewDockX attributes were registered: %s" % ", ".join(sorted(list(attr_names - numeric_attr_names))))
